eb28e34fbd3041b8021763657024e0909cdef253
José Estrella <estrellaquirozjoseluis@gmail.com>
2025-05-10 09:38:50 -0500

    Delete cuadrado.py

--- cuadrado.py
+++
@@ -1,7 +0,0 @@
-class Cuadrado(FiguraGeometrica):
-    def __init__(self, lado):
-        super().__init__(lado, lado)
-    def area(self):
-        return self._alto * self._ancho
-    def __str__(self):
-        return f'Cuadrado de lado {self._alto}, área {self.area()}'
